1cdae2aa67752311eeb551f49dc56ac406b464e0
giovanni-iannaccone <iannacconegiovanni444@gmail.com>
2024-12-08 11:33:08 +0100

    Fix IsProhibited function

--- internal/isProhibited.go
+++ internal/isProhibited.go
@@ -2,7 +2,8 @@ package internals
 
 func IsProhibited(prohibitedList []string, directory []byte) bool {
 	for _, dir := range prohibitedList {
-		if string(directory)[:len(dir)] == dir {
+		if len(dir) < len(directory) && 
+			string(directory)[:len(dir)] == dir {
 			return true
 		}
 	}
