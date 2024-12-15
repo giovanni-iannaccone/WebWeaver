2a85dd82b1e1c7e059544c406e3ccf090c5714a6
giovanni-iannaccone <iannacconegiovanni444@gmail.com>
2024-12-15 16:28:13 +0100

    Add darkmode

--- webui/static/script.js
+++ webui/static/script.js
@@ -1,39 +1,48 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const serverHolder = document.getElementById("server-holder");
-    const socket = new WebSocket("ws://" + document.location.host + "/ws/");
+function disableDarkMode() {
+    document.body.classList.remove("dark-mode");
+    localStorage.setItem("darkMode", null);
+}
 
-    socket.onopen = function () {
-        console.log("OPENED");
-    }
+function enableDarkMode() {
+    document.body.classList.add("dark-mode");
+    localStorage.setItem("darkMode", "active");
+}
 
-    socket.onerror = function (e) {
-        console.log("WebSocket error: ", e);
-    }
+function themeSwitch() {
+    let dark = localStorage.getItem("darkMode");
+    dark == "active"
+        ? disableDarkMode()
+        : enableDarkMode();
+}
 
-    socket.onmessage = function (e) {
-        let html = "";
+const serverHolder = document.getElementById("server-holder");
+const socket = new WebSocket("ws://" + document.location.host + "/ws/");
 
-        const serverData = JSON.parse(e.data)
-        serverData.Data.forEach(server => {
-            const status = server.IsAlive
-                ? `<p><span class="green-circle"}"></span> Online</p>`
-                : `<p><span class="red-circle"}"></span> Offline</p>`;
+socket.onerror = function (e) {
+    console.log("WebSocket error: ", e);
+}
 
-            html += `
-            <div class="server">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 14.25h13.5m-13.5 0a3 3 0 0 1-3-3m3 3a3 3 0 1 0 0 6h13.5a3 3 0 1 0 0-6m-16.5-3a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3m-19.5 0a4.5 4.5 0 0 1 .9-2.7L5.737 5.1a3.375 3.375 0 0 1 2.7-1.35h7.126c1.062 0 2.062.5 2.7 1.35l2.587 3.45a4.5 4.5 0 0 1 .9 2.7m0 0a3 3 0 0 1-3 3m0 3h.008v.008h-.008v-.008Zm0-6h.008v.008h-.008v-.008Zm-3 6h.008v.008h-.008v-.008Zm0-6h.008v.008h-.008v-.008Z" />
-                </svg>
-                
-                <p>${ server.URL }</p>
-                ${status}
-            </div>`;
-        });
+socket.onmessage = function (e) {
+    let html = "";
 
-        serverHolder.innerHTML = html;
-    };
+    const serverData = JSON.parse(e.data)
+    serverData.Data.forEach(server => {
+        const status = server.IsAlive
+            ? `<p><span class="green-circle"}"></span> Online</p>`
+            : `<p><span class="red-circle"}"></span> Offline</p>`;
 
-    socket.onclose = function () {
-        console.log("CLOSED");
-    }
-});
+        html += `
+        <div class="server">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 14.25h13.5m-13.5 0a3 3 0 0 1-3-3m3 3a3 3 0 1 0 0 6h13.5a3 3 0 1 0 0-6m-16.5-3a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3m-19.5 0a4.5 4.5 0 0 1 .9-2.7L5.737 5.1a3.375 3.375 0 0 1 2.7-1.35h7.126c1.062 0 2.062.5 2.7 1.35l2.587 3.45a4.5 4.5 0 0 1 .9 2.7m0 0a3 3 0 0 1-3 3m0 3h.008v.008h-.008v-.008Zm0-6h.008v.008h-.008v-.008Zm-3 6h.008v.008h-.008v-.008Zm0-6h.008v.008h-.008v-.008Z" />
+            </svg>
+            
+            <p>${ server.URL }</p>
+            ${status}
+        </div>`;
+    });
+
+    serverHolder.innerHTML = html;
+};
+
+themeSwitch();
